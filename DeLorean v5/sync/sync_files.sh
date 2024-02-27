7644c916fc89d4af8415d727e98a02b2d03833aa
Dinos <kkorominas@ufl.edu>
2024-02-27 11:01:15 -0500

    rob addition to logging

--- DeLorean v5/sync/sync_files.sh
+++ DeLorean v5/sync/sync_files.sh
@@ -11,12 +11,15 @@ DEST="/Volumes/SFA-All/User Data/$(whoami)/"
 # Rsync options and excludes as arrays
 OPTIONS=(--archive --verbose --partial --progress --stats --delete)
 EXCLUDES=(--exclude='Pictures/Photos Library.photoslibrary' --exclude='.DS_Store')
+exit_codes=""
 
 # Perform backup for each source directory
 for SOURCE in "${SOURCES[@]}"; do
     rsync "${OPTIONS[@]}" "${EXCLUDES[@]}" "$SOURCE" "$DEST"
-    echo "$(date '+%Y-%m-%d %H:%M:%S') error:$?" > "$DEST/dBackup.log"
+    exit_codes="$exit_codes $(basename "$source")-error:$?;"
 done
+echo "$(date '+%Y-%m-%d %H:%M:%S')$exit_codes" >> /sfa-all/user\ data/gatorlink/dBackup.log
+
 
 echo "Backup completed."
 

312faf1fdc63797a5fc903150cffed9fc3233c8e
dinoswork <kkorominas@ufl.edu>
2024-07-01 11:45:51 -0400

    moved latest 'sync.app' to project root

--- sync/sync_files.sh
+++ sync/sync_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Backup scheduling parameters
-scheduledBackupTime="11:09"
+scheduledBackupTime="11:33"
 rangeStart="07:00"
 rangeEnd="21:00"
 # How often the app should check if an rsync happened that day in seconds (3600 seconds = 1 hour)
